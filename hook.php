1a453f8346bfd9db5b5c472b8e0e5f927a7808bc
JimKass <james.kastner@my.jcu.edu.au>
2020-05-25 12:58:17 +1000

    Test 10

--- hook.php
+++ hook.php
@@ -1,7 +1,6 @@
 <?php
-define('PRIVATE_KEY', '');
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_REQUEST['thing'] === PRIVATE_KEY)
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
 {
     file_put_contents('hook_log.txt', 'Request Recieved\n', FILE_APPEND);
     echo shell_exec('sudo git pull');
