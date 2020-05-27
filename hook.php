4049e182ad34669a6a5eefbc92f3ef66d0c30c3e
JimKass <james.kastner@my.jcu.edu.au>
2020-05-27 14:32:10 +1000

    Made hook_log output a little prettier

--- hook.php
+++ hook.php
@@ -1,5 +1,5 @@
 <?php
 shell_exec('rm themes/tjc_theme/style.css');
 $output = shell_exec('git pull');
-file_put_contents('hook_log.txt',$output."\nPull completed: ".date(DATE_RFC2822)."\n",FILE_APPEND);
+file_put_contents('hook_log.txt',$output."\nPull completed: ".date(DATE_RFC2822)."\n\n",FILE_APPEND);
 ?>
