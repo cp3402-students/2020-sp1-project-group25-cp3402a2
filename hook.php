7472110bff65c30a2288cdb9465f8c0341f648a3
JimKass <james.kastner@my.jcu.edu.au>
2020-05-28 12:36:49 +1000

    Making hook.php force a pull to overwrite staging files

--- hook.php
+++ hook.php
@@ -1,5 +1,7 @@
 <?php
 //shell_exec('rm themes/tjc_theme/style.css');
-$output = shell_exec('git pull');
+$output = shell_exec('git fetch --all');
+$output .= shell_exec('git reset --hard origin/master');
+$output .= shell_exec('git pull origin master');
 file_put_contents('hook_log.txt',$output."\nPull completed: ".date(DATE_RFC2822)."\n\n",FILE_APPEND);
 ?>
